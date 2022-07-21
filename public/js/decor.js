29805bb72db43bfefddbdfef457a7e901d15aeb2
Diksheet Agarwal <dksheetagarwal12@gmail.com>
2022-07-21 19:12:12 +0530

    added fashion and decor.js in public

---
+++ public/js/decor.js
@@ -0,0 +1,29 @@
+
+const decor = [
+    {
+        name : "Ganpati Painting",
+        image : "painting.jpg",
+        star : 5,
+        cost_price : 2999,
+        discount_price: 1099,
+        discount_percent : 63
+    },
+    {
+        name : "Wall Clock",
+        image : "clock.jpg",
+        star : 4,
+        cost_price : 1099,
+        discount_price: 499,
+        discount_percent : 55
+    },
+    {
+        name : "Round Table",
+        image : "table.jpg",
+        star : 3,
+        cost_price : 4999,
+        discount_price: 2999,
+        discount_percent : 41
+    }
+]
+
+module.exports = decor;
---
+++ public/js/fashion.js
@@ -0,0 +1,29 @@
+
+const fashion = [
+    {
+        name : "Casual Shirt",
+        image : "shirt.jpg",
+        star : 4,
+        cost_price : 2999,
+        discount_price: 1099,
+        discount_percent : 63
+    },
+    {
+        name : "Casual Pant",
+        image : "pant.jpg",
+        star : 4,
+        cost_price : 1599,
+        discount_price: 999,
+        discount_percent : 38
+    },
+    {
+        name : "Stylish Cap for Men",
+        image : "cap.jpg",
+        star : 3,
+        cost_price : 399,
+        discount_price: 199,
+        discount_percent : 51
+    }
+]
+
+module.exports = fashion;
